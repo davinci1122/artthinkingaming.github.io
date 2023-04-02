49f9d04ed868a6c3eb4c1e16439e1668ac788eea
davinci11110 <64345438+davinci1122@users.noreply.github.com>
2023-04-02 21:57:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const photoDirB = "photosB/";
 const photoOptionsA = ["photo1.jpg", "photo2.jpg", "photo3.jpg", "photo4.jpg", "photo5.jpg",
  "photo6.jpg", "photo7.jpg", "photo8.jpg", "photo9.jpg", "photo10.jpg"];
 const photoOptionsB = ["picture1.jpg", "picture2.jpg", "picture3.jpg"];
-const themeOptions = ["動物の生活をより良くしてください", "使用体験をより豊かにしてください", "より環境に良くしてください",
+const themeOptions = ["動物の生活をより良くしてください", "使用体験をより豊かにしてください", "環境により良くしてください",
 "電気を使わずに、同じような機能を実装してください", "家族全体で共有する利点を追加してください"];
 
 // Get photo elements from HTML
